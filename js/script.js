57b16b56c7684070b6461d02ac7d258d8fed25cc
ejpareja <66528276+ejpareja@users.noreply.github.com>
2020-06-27 14:21:41 -0500

    Add files via upload
    Parcial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function sendMsg () {
+    var m = document.getElementById("usermsg").value;
+
+    document.getElementById("rootmsg").value = "User: "+ m  +  
+    "                                           Root : " + m ;
+    
+}
